cde0c7c2c67d37c9ef261bed31dcb8d132c60b5e
Alexey Nikitin <nikitin@bookmate.com>
2019-02-07 23:21:23 +0300

    Replace function call with macros for clarity and perfomance sake

--- src/miniature_engine_channels.erl
+++ src/miniature_engine_channels.erl
@@ -6,30 +6,28 @@
     all/0
 ]).
 
+-define(KEY(Key), {
+    p, %% type = property
+    l, %% scope = local
+    {channel, Key}
+}).
+
 -spec register(binary()) -> true.
 register(Key) ->
-    true = gproc:reg(build_key(Key)).
+    true = gproc:reg(?KEY(Key)).
 
 -spec by_key(binary()) -> [pid()].
 by_key(Key) ->
-    gproc:lookup_pids(build_key(Key)).
+    gproc:lookup_pids(?KEY(Key)).
 
 -spec all() -> [pid()].
 all() ->
     gproc:select(_SelectContext = {l, p}, _MatchFunction = [{
-        {                    %% MatchHead begins
-            build_key('$1'), %% Key   -> $1
-            '$2',            %% Pid   -> $2
-            '$3'             %% Props -> $3
-        },                   %% MatchHead ends
-        [],                  %% [Guard]
-        ['$2']               %% [Result]
+        {               %% MatchHead begins
+            ?KEY('$1'), %% Key   -> $1
+            '$2',       %% Pid   -> $2
+            '$3'        %% Props -> $3
+        },              %% MatchHead ends
+        [],             %% [Guard]
+        ['$2']          %% [Result]
     }]).
-
--spec build_key(binary() | '$1') -> gproc:key().
-build_key(Key) ->
-    {
-        p, %% type = property
-        l, %% scope = local
-        {channel, Key}
-    }.
